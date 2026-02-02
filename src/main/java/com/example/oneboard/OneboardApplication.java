fb409b98823cf53bc97b468ca24571d7ee9f1dd3
Jung, Hwak <junghwak56@gmail.com>
2026-02-02 10:37:06 +0900

    Initial commit

---
+++ src/main/java/com/example/oneboard/OneboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oneboard/OneboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
